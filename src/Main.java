7f547c6994ad1138c03e97089bd78e16b3102714
gauravkmishra09 <mishragauravkumar2005@gmail.com>
2026-05-05 04:32:49 +0530

    THIS IS BASIC ATM MACHINE

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int pin=in.nextInt();
         if((atmnumber==atmNumber)&&(atmpin==pin)){
             while(true){
-                System.out.println("1.View Available Balance\n2.Withdraw Amount\n3.Deposit Amount\n4.View Ministatement\n5.Exit");
+                System.out.println("1.View Available Balance\n2.Withdraw Amount \n3.Deposit Amount\n4.View Ministatement\n5.Exit");
                 System.out.println("Enter Choice : ");
                 int ch=in.nextInt();
                 if(ch==1){
